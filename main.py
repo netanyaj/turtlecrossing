039bea3d248b0f574774c10c8e717c075e46e0d9
netanyaj <netanyajaison@gmail.com>
2023-05-30 17:03:19 +0530

    Initial commit

---
+++ main.py
@@ -0,0 +1,38 @@
+import time
+from turtle import Screen, Turtle
+from player import Player
+from car_manager import CarManager
+from scoreboard import Scoreboard
+
+screen = Screen()
+screen.setup(width=600, height=600)
+screen.title("turtle crossing 🛣️")
+screen.tracer(0)
+player = Player()
+car = CarManager()
+car.hideturtle()
+scoreboard = Scoreboard()
+
+screen.listen()
+screen.onkeypress(player.move, "Up")
+
+game_is_on = True
+while game_is_on:
+    time.sleep(0.1)
+    screen.update()
+    car.create_car()
+    car.move_car()
+
+    # Detect collision with cars
+    for a in car.all_cars:
+        if player.distance(a) < 20:
+            game_is_on = False
+            scoreboard.game_over()
+
+    # Detect successful crossing
+    if player.is_at_finish_line():
+        player.go_to_start()
+        scoreboard.update()
+        car.level_up()
+
+screen.exitonclick()
